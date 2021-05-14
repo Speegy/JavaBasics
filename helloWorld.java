36aaa61654a4552ddaf31a0250d297ec111e4024
Speegy <mcjakedude@gmail.com>
2021-05-14 13:56:09 -0600

    hello world and basic math methods

---
+++ helloWorld.java
@@ -0,0 +1,10 @@
+public class HelloWorld{
+    public static void main(String[] args) {
+        printMessage();
+    }
+
+    public static void printMessage() {
+        String message = "Hello World!";
+        System.out.println(message);
+    }
+}
